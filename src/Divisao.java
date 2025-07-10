5966d4fb6f30a297d192408593ec8417264466d3
RODRIGO BERGENTHAL <116042656+RodrigoBergenthal@users.noreply.github.com>
2025-07-10 19:15:37 -0300

    Add files via upload
    add

---
+++ src/Divisao.java
@@ -0,0 +1,5 @@
+public class Divisao {
+    public static void main(String[] args) {
+        System.out.println(10/5);
+    }
+}
---
+++ src/Multiplicacao.java
@@ -0,0 +1,5 @@
+public class Multiplicacao {
+    public static void main(String[] args) {
+        System.out.println(10*5);
+    }
+}
